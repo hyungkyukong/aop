21045968c32d6b46176175feddad2b94b87f3c27
hkkong <konguuu@gmail.com>
2023-10-22 23:23:20 +0900

    ### 스프링 AOP - 실전 예제 예제 만들기

---
+++ src/main/java/hello/aop/order/aop/exam/ExamService.java
@@ -0,0 +1,15 @@
+package hello.aop.order.aop.exam;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+}
